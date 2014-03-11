b944b941fd2f2482e27f75b10b52205eff491c4a
Mathias Buus <mathiasbuus@gmail.com>
2014-03-12 00:14:10 +0100

    using 256mb buffers for less fd open/close

--- storage.js
+++ storage.js
@@ -4,11 +4,19 @@ var mkdirp = require('mkdirp');
 var rimraf = require('rimraf');
 var thunky = require('thunky');
 var events = require('events');
+var raf = require('random-access-file');
 
 var noop = function() {};
 
 module.exports = function(folder, torrent) {
 	var that = new events.EventEmitter();
+
+	var bufferSize = torrent.pieceLength;
+	while (bufferSize < 256 * 1024 * 1024) bufferSize *= 2;
+
+	var pieceLength = torrent.pieceLength;
+	var pieceRemainder = (torrent.length % pieceLength) || pieceLength;
+	var piecesPerBuffer = bufferSize / pieceLength;
 
 	var mkdir = thunky(function(cb) {
 		mkdirp(folder, cb);
@@ -18,10 +26,18 @@ module.exports = function(folder, torrent) {
 		return '00000000000'.slice(0, 10-(''+i).length)+i;
 	};
 
+	var files = [];
+
 	that.read = function(index, cb) {
 		mkdir(function(err) {
 			if (err) return cb(err);
-			fs.readFile(path.join(folder, pad(index)), cb);
+
+			var i = (index / piecesPerBuffer) | 0;
+			var offset = index - i * piecesPerBuffer;
+			var len = index === torrent.pieces.length-1 ? pieceRemainder : pieceLength;
+			var file = files[i] = files[i] || raf(path.join(folder, pad(i)));
+
+			file.read(offset * pieceLength, len, cb);
 		});
 	};
 
@@ -29,12 +45,22 @@ module.exports = function(folder, torrent) {
 		if (!cb) cb = noop;
 		mkdir(function(err) {
 			if (err) return cb(err);
-			fs.writeFile(path.join(folder, pad(index)), buffer, cb);
+
+			var i = (index / piecesPerBuffer) | 0;
+			var file = files[i] = files[i] || raf(path.join(folder, pad(i)));
+			var offset = index - i * piecesPerBuffer;
+
+			file.write(offset * pieceLength, buffer, cb);
 		});
 	};
 
 	that.destroy = function(cb) {
 		if (!cb) cb = noop;
+
+		files.forEach(function(file) {
+			file.close();
+		});
+
 		mkdir(function(err) {
 			if (err) return cb(err);
 			rimraf(folder, cb);
