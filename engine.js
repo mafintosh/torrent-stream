8d6148426b75cc01522031f3b0eae3766feb7391
Gleb Arestov <gr1b0k@gmail.com>
2014-03-19 00:34:04 +0400

    handshake option rename

--- engine.js
+++ engine.js
@@ -66,7 +66,7 @@ var engine = function(torrent, opts) {
 	var that = new events.EventEmitter();
 	var swarm = peerWireSwarm(torrent.infoHash, opts.id, {
 		size: opts.connections || opts.size,
-		handshake_options: {
+		handshake: {
 			extension_protocol: true
 		}
 	});
