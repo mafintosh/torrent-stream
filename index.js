e1428085972bce7e9a6eeb01a1a0a63960fd763f
Mathias Buus <mathiasbuus@gmail.com>
2014-03-19 22:07:47 +0100

    restructure and added magnet support

--- index.js
+++ index.js
@@ -1,48 +1,534 @@
-var engine = require('./engine');
-var dht = require('./dht');
-var fileStream = require('./file-stream');
+var magnet = require('magnet-uri');
+var hat = require('hat');
+var pws = require('peer-wire-swarm');
+var bncode = require('bncode');
+var crypto = require('crypto');
+var bitfield = require('bitfield');
 var parseTorrent = require('parse-torrent');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+var events = require('events');
+var path = require('path');
+var fs = require('fs');
 var eos = require('end-of-stream');
+var storage = require('./storage');
+var fileStream = require('./file-stream');
+var piece = require('./piece');
+var dht = require('./dht');
 
-module.exports = function(torrent, opts) {
-	torrent = !Buffer.isBuffer(torrent) && typeof torrent === 'object' ? torrent : parseTorrent(torrent);
+var MAX_REQUESTS = 5;
+var CHOKE_TIMEOUT = 5000;
+var REQUEST_TIMEOUT = 30000;
+var SPEED_THRESHOLD = 3 * piece.BLOCK_SIZE;
+var META_BLOCK_SIZE = 1 << 14;
 
-	var e = engine(torrent, opts);
+var EXTENSIONS = {
+	m: {
+		ut_metadata: 1
+	}
+};
 
-	e.files = torrent.files.map(function(file) {
-		var offsetPiece = (file.offset / torrent.pieceLength) | 0;
-		var endPiece = ((file.offset+file.length-1) / torrent.pieceLength) | 0;
+var noop = function() {};
 
-		file.deselect = function() {
-			e.deselect(offsetPiece, endPiece, false);
-		};
+var sha1 = function(data) {
+	return crypto.createHash('sha1').update(data).digest('hex');
+};
 
-		file.select = function() {
-			e.select(offsetPiece, endPiece, false);
-		};
+var thruthy = function() {
+	return true;
+};
 
-		file.createReadStream = function(opts) {
-			var self = this;
-			var stream = fileStream(e, file, opts);
+var falsy = function() {
+	return false;
+};
 
-			e.select(stream.startPiece, stream.endPiece, true, stream.notify.bind(stream));
-			eos(stream, function() {
-				e.deselect(stream.startPiece, stream.endPiece, true);
+var toNumber = function(val) {
+	return val === true ? 1 : (val || 0);
+};
+
+var torrentStream = function(link, opts) {
+	link = magnet(link);
+	var infoHash = link.infoHash;
+
+	if (!opts) opts = {};
+	if (!opts.id) opts.id = '-PF0007-'+hat(48);
+	if (!opts.path) opts.path = path.join(opts.tmp || '/tmp', 'torrent-stream', infoHash);
+
+	var engine = new events.EventEmitter();
+	var swarm = pws(infoHash, opts.id, {size:opts.connections || opts.size});
+
+	var wires = swarm.wires;
+	var critical = [];
+	var metadataPieces = [];
+	var metadata = null;
+	var store = null;
+	var refresh = noop;
+
+	engine.files = [];
+	engine.selection = [];
+	engine.torrent = null;
+	engine.bitfield = null;
+	engine.amInterested = false;
+	engine.swarm = swarm;
+
+	if (opts.dht !== false) {
+		var table = dht(infoHash);
+		engine.dht = table;
+		table.on('peer', function(addr) {
+			engine.emit('peer', addr);
+			engine.connect(addr);
+		});
+	}
+
+	var ontorrent = function(torrent) {
+		store = storage(opts.path, torrent);
+
+		engine.torrent = torrent;
+		engine.bitfield = bitfield(torrent.pieces.length);
+
+		var pieceLength = torrent.pieceLength;
+		var pieceRemainder = (torrent.length % pieceLength) || pieceLength;
+
+		var pieces = torrent.pieces.map(function(hash, i) {
+			return piece(i === torrent.pieces.length-1 ? pieceRemainder : pieceLength);
+		});
+		var reservations = torrent.pieces.map(function() {
+			return [];
+		});
+
+		torrent.files.forEach(function(file) {
+			var offsetPiece = (file.offset / torrent.pieceLength) | 0;
+			var endPiece = ((file.offset+file.length-1) / torrent.pieceLength) | 0;
+
+			file.deselect = function() {
+				engine.deselect(offsetPiece, endPiece, false);
+			};
+
+			file.select = function() {
+				engine.select(offsetPiece, endPiece, false);
+			};
+
+			file.createReadStream = function(opts) {
+				var self = this;
+				var stream = fileStream(engine, file, opts);
+
+				engine.select(stream.startPiece, stream.endPiece, true, stream.notify.bind(stream));
+				eos(stream, function() {
+					engine.deselect(stream.startPiece, stream.endPiece, true);
+				});
+
+				return stream;
+			};
+
+			engine.files.push(file);
+		});
+
+		var oninterestchange = function() {
+			var prev = engine.amInterested;
+			engine.amInterested = !!engine.selection.length;
+
+			wires.forEach(function(wire) {
+				if (engine.amInterested) wire.interested();
+				else wire.uninterested();
 			});
 
-			return stream;
+			if (prev === engine.amInterested) return;
+			if (engine.amInterested) engine.emit('interested');
+			else engine.emit('uninterested');
 		};
 
-		return file;
+		var gc = function() {
+			for (var i = 0; i < engine.selection.length; i++) {
+				var s = engine.selection[i];
+				var oldOffset = s.offset;
+
+				while (!pieces[s.from+s.offset] && s.from+s.offset < s.to) s.offset++;
+
+				if (oldOffset !== s.offset) s.notify();
+				if (s.to !== s.from+s.offset) continue;
+				if (pieces[s.from+s.offset]) continue;
+
+				engine.selection.splice(i, 1);
+				i--; // -1 to offset splice
+				s.notify();
+				oninterestchange();
+			}
+
+			if (!engine.selection.length) engine.emit('idle');
+		};
+
+		var onpiececomplete = function(index, buffer) {
+			if (!pieces[index]) return;
+
+			pieces[index] = null;
+			reservations[index] = null;
+			engine.bitfield.set(index, true);
+
+			for (var i = 0; i < wires.length; i++) wires[i].have(index);
+
+			engine.emit('verify', index);
+			engine.emit('download', index, buffer);
+
+			store.write(index, buffer);
+			gc();
+		};
+
+		var onhotswap = opts.hotswap === false ? falsy : function(wire, index) {
+			var speed = wire.downloadSpeed();
+			if (speed < piece.BLOCK_SIZE) return;
+			if (!reservations[index] || !pieces[index]) return;
+
+			var r = reservations[index];
+			var minSpeed = Infinity;
+			var min;
+
+			for (var i = 0; i < r.length; i++) {
+				var other = r[i];
+				if (!other || other === wire) continue;
+
+				var otherSpeed = other.downloadSpeed();
+				if (otherSpeed >= SPEED_THRESHOLD) continue;
+				if (2 * otherSpeed > speed || otherSpeed > minSpeed) continue;
+
+				min = other;
+				minSpeed = otherSpeed;
+			}
+
+			if (!min) return false;
+
+			for (var i = 0; i < r.length; i++) {
+				if (r[i] === min) r[i] = null;
+			}
+
+			for (var i = 0; i < min.requests.length; i++) {
+				var req = min.requests[i];
+				if (req.piece !== index) continue;
+				pieces[index].cancel((req.offset / piece.BLOCK_SIZE) | 0);
+			}
+
+			engine.emit('hotswap', min, wire, index);
+			return true;
+		};
+
+		var onupdatetick = function() {
+			process.nextTick(onupdate);
+		};
+
+		var onrequest = function(wire, index, hotswap) {
+			if (!pieces[index]) return false;
+
+			var p = pieces[index];
+			var reservation = p.reserve();
+
+			if (reservation === -1 && hotswap && onhotswap(wire, index)) reservation = p.reserve();
+			if (reservation === -1) return false;
+
+			var r = reservations[index] || [];
+			var offset = p.offset(reservation);
+			var size = p.size(reservation);
+
+			var i = r.indexOf(null);
+			if (i === -1) i = r.length;
+			r[i] = wire;
+
+			wire.request(index, offset, size, function(err, block) {
+				if (r[i] === wire) r[i] = null;
+
+				if (p !== pieces[index]) return onupdatetick();
+
+				if (err) {
+					p.cancel(reservation);
+					onupdatetick();
+					return;
+				}
+
+				if (!p.set(reservation, block)) return onupdatetick();
+
+				var buffer = p.flush();
+
+				if (sha1(buffer) !== torrent.pieces[index]) {
+					pieces[index] = piece(p.length);
+					engine.emit('invalid-piece', index, buffer);
+					onupdatetick();
+					return;
+				}
+
+				onpiececomplete(index, buffer);
+				onupdatetick();
+			});
+
+			return true;
+		};
+
+		var onvalidatewire = function(wire) {
+			if (wire.requests.length) return;
+
+			for (var i = engine.selection.length-1; i >= 0; i--) {
+				var next = engine.selection[i];
+				for (var j = next.to; j >= next.from + next.offset; j--) {
+					if (!wire.peerPieces[j]) continue;
+					if (onrequest(wire, j, false)) return;
+				}
+			}
+		};
+
+		var speedRanker = function(wire) {
+			var speed = wire.downloadSpeed() || 1;
+			if (speed > SPEED_THRESHOLD) return thruthy;
+
+			var secs = MAX_REQUESTS * piece.BLOCK_SIZE / speed;
+			var tries = 10;
+			var ptr = 0;
+
+			return function(index) {
+				if (!tries || !pieces[index]) return true;
+
+				var missing = pieces[index].missing;
+				for (; ptr < wires.length; ptr++) {
+					var other = wires[ptr];
+					var otherSpeed = other.downloadSpeed();
+
+					if (otherSpeed < speed || !other.peerPieces[index]) continue;
+					if (missing -= otherSpeed * secs > 0) continue;
+
+					tries--;
+					return false;
+				}
+
+				return true;
+			};
+		};
+
+		var select = function(wire, hotswap) {
+			if (wire.requests.length >= MAX_REQUESTS) return true;
+
+			var rank = speedRanker(wire);
+
+			for (var i = 0; i < engine.selection.length; i++) {
+				var next = engine.selection[i];
+				for (var j = next.from + next.offset; j <= next.to; j++) {
+					if (!wire.peerPieces[j] || !rank(j)) continue;
+					while (wire.requests.length < MAX_REQUESTS && onrequest(wire, j, critical[j] || hotswap));
+					if (wire.requests.length >= MAX_REQUESTS) return true;
+				}
+			}
+
+			return false;
+		};
+
+		var onupdatewire = function(wire) {
+			if (wire.peerChoking) return;
+			if (!wire.downloaded) return onvalidatewire(wire);
+			select(wire, false) || select(wire, true);
+		};
+
+		var onupdate = function() {
+			wires.forEach(onupdatewire);
+		};
+
+		var onwire = function(wire) {
+			wire.setTimeout(opts.timeout || REQUEST_TIMEOUT, function() {
+				engine.emit('timeout', wire);
+				wire.destroy();
+			});
+
+			if (engine.selection.length) wire.interested();
+
+			var timeout = CHOKE_TIMEOUT;
+			var id;
+
+			var onchoketimeout = function() {
+				if (swarm.queued > 2 * (swarm.size - swarm.wires.length) && wire.amInterested) return wire.destroy();
+				id = setTimeout(onchoketimeout, timeout);
+			};
+
+			wire.on('close', function() {
+				clearTimeout(id);
+			});
+
+			wire.on('choke', function() {
+				clearTimeout(id);
+				id = setTimeout(onchoketimeout, timeout);
+			});
+
+			wire.on('unchoke', function() {
+				clearTimeout(id);
+			});
+
+			wire.on('request', function(index, offset, length, cb) {
+				if (pieces[index]) return;
+				store.read(index, function(err, buffer) {
+					if (err) return cb(err);
+					engine.emit('upload', index, offset, length);
+					cb(null, buffer.slice(offset, offset+length));
+				});
+			});
+
+			wire.on('unchoke', onupdate);
+			wire.on('bitfield', onupdate);
+			wire.on('have', onupdate);
+
+			wire.once('interested', function() {
+				wire.unchoke();
+			});
+
+			id = setTimeout(onchoketimeout, timeout);
+		};
+
+		var onready = function() {
+			swarm.on('wire', onwire);
+			swarm.wires.forEach(onwire);
+
+			refresh = function() {
+				process.nextTick(gc);
+				oninterestchange();
+				onupdate();
+			};
+
+			engine.emit('ready');
+			refresh();
+		};
+
+		if (opts.verify === false) return onready();
+
+		engine.emit('verifying');
+
+		var loop = function(i) {
+			if (i >= torrent.pieces.length) return onready();
+			store.read(i, function(err, buf) {
+				if (!buf || sha1(buf) !== torrent.pieces[i] || !pieces[i]) return loop(i+1);
+				pieces[i] = null;
+				engine.bitfield.set(i, true);
+				engine.emit('verify', i);
+				loop(i+1);
+			});
+		};
+
+		loop(0);
+	};
+
+	var onmetadata = function(buf, size) {
+		var delimiter = buf.toString('ascii').indexOf('ee')+2;
+		var message = bncode.decode(buf.slice(0, delimiter));
+
+		if (message.msg_type === 2) return;
+
+		if (message.msg_type === 1 && !metadata) {
+			metadataPieces[message.piece] = buf.slice(delimiter);
+			for (var i = 0; i * META_BLOCK_SIZE < size; i++) {
+				if (!metadataPieces[i]) return;
+			}
+
+			metadata = Buffer.concat(metadataPieces);
+
+			if (infoHash !== sha1(metadata)) {
+				metadataPieces = [];
+				metadata = null;
+				return;
+			}
+
+			var result = {};
+			result.info = bncode.decode(metadata);
+			result['announce-list'] = [];
+
+			var torrent = parseTorrent(bncode.encode(result));
+
+			ontorrent(torrent);
+		}
+	};
+
+	swarm.on('wire', function(wire) {
+		engine.emit('wire', wire);
+
+		wire.once('extended', function(id, handshake) {
+			handshake = bncode.decode(handshake);
+
+			if (id || !handshake.m || handshake.m.ut_metadata === undefined) return;
+
+			var mt = handshake.m.ut_metadata;
+			var size = handshake.metadata_size;
+
+			wire.on('extended', function(id, ext) {
+				if (id === EXTENSIONS.m.ut_metadata) onmetadata(ext, size);
+			});
+
+			if (!size || metadata) return;
+
+			for (var i = 0; i * META_BLOCK_SIZE < size; i++) {
+				if (metadataPieces[i]) continue;
+				wire.extended(mt, {msg_type:0, piece:i});
+			}
+		});
+
+		if (engine.bitfield) wire.bitfield(engine.bitfield);
+		if (!wire.peerExtensions.extended) return;
+
+		wire.extended(0, {m:{ut_metadata:1}});
 	});
 
-	if (opts.dht === false) return e;
-
-	var table = dht(torrent.infoHash);
-
-	table.on('peer', function(addr) {
-		e.connect(addr);
+	swarm.pause();
+	mkdirp(opts.path, function(err) {
+		if (err) return engine.emit('error', err);
+		swarm.resume();
 	});
 
-	return e;
+	engine.critical = function(piece, width) {
+		for (var i = 0; i < (width || 1); i++) critical[piece+i] = true;
+	};
+
+	engine.select = function(from, to, priority, notify) {
+		engine.selection.push({
+			from:from,
+			to:to,
+			offset:0,
+			priority: toNumber(priority),
+			notify: notify || noop
+		});
+
+		engine.selection.sort(function(a, b) {
+			return b.priority - a.priority;
+		});
+
+		refresh();
+	};
+
+	engine.deselect = function(from, to, priority) {
+		for (var i = 0; i < engine.selection.length; i++) {
+			var s = engine.selection[i];
+			if (s.from !== from || s.to !== to) continue;
+			if (s.priority !== toNumber(priority)) continue;
+			engine.selection.splice(i, 1);
+			i--;
+			break;
+		}
+
+		refresh();
+	};
+
+	engine.connect = function(addr) {
+		swarm.add(addr);
+	};
+
+	engine.disconnect = function(addr) {
+		swarm.remove(addr);
+	};
+
+	engine.remove = function(cb) {
+		rimraf(opts.path, cb);
+	};
+
+	engine.destroy = function() {
+		swarm.destroy();
+		if (engine.dht) engine.dht.destroy();
+		if (store) store.destroy();
+	};
+
+	engine.listen = function(port, cb) {
+		if (typeof port === 'function') return that.listen(0, port);
+		swarm.listen(port || 6881, cb);
+	};
+
+	return engine;
 };
+
+module.exports = torrentStream;
