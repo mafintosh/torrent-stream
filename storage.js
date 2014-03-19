e3b48440eea19cecb32bb6b16cff2d467afa5159
Mathias Buus <mathiasbuus@gmail.com>
2014-03-19 22:08:21 +0100

    simpler storage and inlined memcache

--- storage.js
+++ storage.js
@@ -1,15 +1,11 @@
 var fs = require('fs');
 var path = require('path');
-var mkdirp = require('mkdirp');
-var rimraf = require('rimraf');
-var thunky = require('thunky');
-var events = require('events');
 var raf = require('random-access-file');
 
 var noop = function() {};
 
 module.exports = function(folder, torrent) {
-	var that = new events.EventEmitter();
+	var that = {};
 
 	var bufferSize = torrent.pieceLength;
 	while (bufferSize < 256 * 1024 * 1024) bufferSize *= 2;
@@ -17,54 +13,53 @@ module.exports = function(folder, torrent) {
 	var pieceLength = torrent.pieceLength;
 	var pieceRemainder = (torrent.length % pieceLength) || pieceLength;
 	var piecesPerBuffer = bufferSize / pieceLength;
-
-	var mkdir = thunky(function(cb) {
-		mkdirp(folder, cb);
-	});
+	var mem = [];
+	var files = [];
 
 	var pad = function(i) {
 		return '00000000000'.slice(0, 10-(''+i).length)+i;
 	};
 
-	var files = [];
-
 	that.read = function(index, cb) {
-		mkdir(function(err) {
-			if (err) return cb(err);
+		if (mem[index]) return cb(null, mem[index]);
 
-			var i = (index / piecesPerBuffer) | 0;
-			var offset = index - i * piecesPerBuffer;
-			var len = index === torrent.pieces.length-1 ? pieceRemainder : pieceLength;
-			var file = files[i] = files[i] || raf(path.join(folder, pad(i)));
+		var i = (index / piecesPerBuffer) | 0;
+		var offset = index - i * piecesPerBuffer;
+		var len = index === torrent.pieces.length-1 ? pieceRemainder : pieceLength;
+		var file = files[i] = files[i] || raf(path.join(folder, pad(i)));
 
-			file.read(offset * pieceLength, len, cb);
-		});
+		file.read(offset * pieceLength, len, cb);
 	};
 
 	that.write = function(index, buffer, cb) {
 		if (!cb) cb = noop;
-		mkdir(function(err) {
-			if (err) return cb(err);
 
-			var i = (index / piecesPerBuffer) | 0;
-			var file = files[i] = files[i] || raf(path.join(folder, pad(i)));
-			var offset = index - i * piecesPerBuffer;
+		mem[index] = buffer;
 
-			file.write(offset * pieceLength, buffer, cb);
-		});
+		var ondone = function(err) {
+			mem[index] = null;
+			cb(err);
+		};
+
+		var i = (index / piecesPerBuffer) | 0;
+		var file = files[i] = files[i] || raf(path.join(folder, pad(i)));
+		var offset = index - i * piecesPerBuffer;
+
+		file.write(offset * pieceLength, buffer, ondone);
 	};
 
-	that.destroy = function(cb) {
+	that.close = function(cb) {
 		if (!cb) cb = noop;
 
-		files.forEach(function(file) {
-			file.close();
-		});
-
-		mkdir(function(err) {
+		var i = 0;
+		var loop = function(err) {
 			if (err) return cb(err);
-			rimraf(folder, cb);
-		});
+			var next = files[i++];
+			if (!next) return cb();
+			file.close(loop);
+		};
+
+		loop();
 	};
 
 	return that;
