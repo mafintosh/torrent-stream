8cbc4cf941a7a515e50ca714944eeea41b7e70f3
Aliaksei Sapach <aliaksei.dreamsonic@gmail.com>
2014-05-18 22:48:46 +0300

    added tests for alternate trackers

--- test/tracker.js
+++ test/tracker.js
@@ -8,7 +8,7 @@ var tracker = require('bittorrent-tracker');
 var server = new tracker.Server();
 
 var torrent = parseTorrent(fs.readFileSync(path.join(__dirname, 'data', 'test.torrent')));
-var tmpPath = path.join(__dirname, '..', 'torrents');
+var tmpPath = path.join(__dirname, '..', 'torrents', 'test');
 rimraf.sync(tmpPath);
 
 var fixture = torrents(torrent, {
@@ -23,19 +23,19 @@ server.on('error', function () {
 test('seed should connect to the tracker', function (t) {
 	t.plan(3);
 
-	server.on('listening', t.ok.bind(t, true, 'tracker should be listening'));
+	server.once('listening', t.ok.bind(t, true, 'tracker should be listening'));
 	server.once('start', function (addr) {
 		t.equal(addr, '127.0.0.1:6882');
 	});
 	server.listen(12345);
 
-	fixture.on('ready', t.ok.bind(t, true, 'should be ready'));
+	fixture.once('ready', t.ok.bind(t, true, 'should be ready'));
 });
 
 test('peer should connect to the swarm using .torrent file', function (t) {
 	t.plan(4);
-	var engine = torrents(torrent, { dht: false, tmp: tmpPath });
-	engine.on('ready', function () {
+	var engine = torrents(torrent, { dht: false });
+	engine.once('ready', function () {
 		t.ok(true, 'should be ready');
 		engine.destroy(function() {
 			engine.remove(t.ok.bind(t, true, 'should be destroyed'));
@@ -53,7 +53,7 @@ test('peer should connect to the swarm using magnet link', function (t) {
 	t.plan(4);
 	var engine = torrents('magnet:?xt=urn:btih:1cb9681dccbe6ef86ac797ab93840a4f0c4ccae8' +
 		'&tr=http%3A%2F%2F127.0.0.1%3A12345%2Fannounce', { dht: false, tmp: tmpPath });
-	engine.on('ready', function () {
+	engine.once('ready', function () {
 		t.ok(true, 'should be ready');
 		engine.destroy(function() {
 			engine.remove(t.ok.bind(t, true, 'should be destroyed'));
@@ -72,7 +72,7 @@ test('peer should connect to the swarm using magnet link and trackers', function
 	t.plan(4);
 	var engine = torrents('magnet:?xt=urn:btih:1cb9681dccbe6ef86ac797ab93840a4f0c4ccae8',
 		{ dht: false, tmp: tmpPath, trackers: ['http://127.0.0.1:12345/announce'] });
-	engine.on('ready', function () {
+	engine.once('ready', function () {
 		t.ok(true, 'should be ready');
 		engine.destroy(function() {
 			engine.remove(t.ok.bind(t, true, 'should be destroyed'));
@@ -84,6 +84,23 @@ test('peer should connect to the swarm using magnet link and trackers', function
 	});
 	server.once('stop', function (addr) {
 		t.equal(addr, '127.0.0.1:6881');
+	});
+});
+
+test('peer should connect to an alternate tracker', function (t) {
+	t.plan(5);
+	var server = new tracker.Server();
+	server.once('listening', t.ok.bind(t, true, 'tracker should be listening'));
+	server.once('start', function (addr) {
+		t.equal(addr, '127.0.0.1:6881');
+	});
+	server.listen(54321);
+
+	var engine = torrents(torrent, { dht: false, trackers: ['http://127.0.0.1:54321/announce'] });
+	engine.once('ready', function () {
+		t.ok(true, 'should be ready');
+		engine.destroy(t.ok.bind(t, true, 'should be destroyed'));
+		server.close(t.ok.bind(t, true, 'tracker should be closed'));
 	});
 });
 
