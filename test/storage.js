77dbda099782bafc5fc6b6d4e01056acf9456d39
Feross Aboukhadijeh <feross@feross.org>
2015-08-20 15:37:37 +0200

    use standard

--- test/storage.js
+++ test/storage.js
@@ -1,77 +1,77 @@
-var test = require('tap').test;
-var torrents = require('../');
-var fs = require('fs');
-var path = require('path');
+var test = require('tap').test
+var torrents = require('../')
+var fs = require('fs')
+var path = require('path')
 
-var torrent = fs.readFileSync(path.join(__dirname, 'data', 'test.torrent'));
+var torrent = fs.readFileSync(path.join(__dirname, 'data', 'test.torrent'))
 
 var fixture = torrents(torrent, {
-	dht: false,
-	tracker: false,
-	path: path.join(__dirname, 'data')
-});
+  dht: false,
+  tracker: false,
+  path: path.join(__dirname, 'data')
+})
 
-test('fixture can verify the torrent', function(t) {
-	t.plan(2);
-	fixture.once('ready', function() {
-		t.ok(true, 'should be ready');
-		t.deepEqual(fixture.bitfield.buffer.toString('hex'), 'c0', 'should verify all the pieces');
-	});
-});
+test('fixture can verify the torrent', function (t) {
+  t.plan(2)
+  fixture.once('ready', function () {
+    t.ok(true, 'should be ready')
+    t.deepEqual(fixture.bitfield.buffer.toString('hex'), 'c0', 'should verify all the pieces')
+  })
+})
 
-test('fixture can read the file contents', function(t) {
-	t.equal(fixture.files.length, 1, 'should have one file');
-	var file = fixture.files[0];
-	t.test('can read from stream', function(t) {
-		var stream = file.createReadStream();
-		stream.setEncoding('ascii');
-		t.plan(1);
-		stream.once('readable', function() {
-			t.equal(stream.read(11), 'Lorem ipsum');
-		});
-	});
-	t.test('can read from stream with offset', function(t) {
-		var stream = file.createReadStream({start: 36109});
-		stream.setEncoding('ascii');
-		t.plan(1);
-		stream.once('readable', function() {
-			t.equal(stream.read(6), 'amet. ');
-		});
-	});
-	t.test('can read from storage', function(t) {
-		t.plan(6);
-		fixture.store.get(0, function(err, buffer) {
-			t.equal(buffer.length, 32768);
-			t.equal(buffer.toString('ascii', 0, 11), 'Lorem ipsum');
-		});
-		fixture.store.get(0, function(err, buffer) {
-			t.equal(buffer.length, 32768);
-			t.equal(buffer.toString('ascii', 588, 598), 'Vestibulum');
-		});
-		fixture.store.get(1, function(err, buffer) {
-			t.equal(buffer.length, 3347);
-			t.equal(buffer.toString('ascii', 3341), 'amet. ');
-		});
-	});
-	t.test('can read from storage with offset', function(t) {
-		t.plan(6);
-		fixture.store.get(0, {length: 11}, function(err, buffer) {
-			t.equal(buffer.length, 11);
-			t.equal(buffer.toString('ascii'), 'Lorem ipsum');
-		});
-		fixture.store.get(0, {offset: 588, length: 10}, function(err, buffer) {
-			t.equal(buffer.length, 10);
-			t.equal(buffer.toString('ascii'), 'Vestibulum');
-		});
-		fixture.store.get(1, {offset: 3341}, function(err, buffer) {
-			t.equal(buffer.length, 6);
-			t.equal(buffer.toString('ascii'), 'amet. ');
-		});
-	});
-	t.end();
-});
+test('fixture can read the file contents', function (t) {
+  t.equal(fixture.files.length, 1, 'should have one file')
+  var file = fixture.files[0]
+  t.test('can read from stream', function (t) {
+    var stream = file.createReadStream()
+    stream.setEncoding('ascii')
+    t.plan(1)
+    stream.once('readable', function () {
+      t.equal(stream.read(11), 'Lorem ipsum')
+    })
+  })
+  t.test('can read from stream with offset', function (t) {
+    var stream = file.createReadStream({start: 36109})
+    stream.setEncoding('ascii')
+    t.plan(1)
+    stream.once('readable', function () {
+      t.equal(stream.read(6), 'amet. ')
+    })
+  })
+  t.test('can read from storage', function (t) {
+    t.plan(6)
+    fixture.store.get(0, function (_, buffer) {
+      t.equal(buffer.length, 32768)
+      t.equal(buffer.toString('ascii', 0, 11), 'Lorem ipsum')
+    })
+    fixture.store.get(0, function (_, buffer) {
+      t.equal(buffer.length, 32768)
+      t.equal(buffer.toString('ascii', 588, 598), 'Vestibulum')
+    })
+    fixture.store.get(1, function (_, buffer) {
+      t.equal(buffer.length, 3347)
+      t.equal(buffer.toString('ascii', 3341), 'amet. ')
+    })
+  })
+  t.test('can read from storage with offset', function (t) {
+    t.plan(6)
+    fixture.store.get(0, {length: 11}, function (_, buffer) {
+      t.equal(buffer.length, 11)
+      t.equal(buffer.toString('ascii'), 'Lorem ipsum')
+    })
+    fixture.store.get(0, {offset: 588, length: 10}, function (_, buffer) {
+      t.equal(buffer.length, 10)
+      t.equal(buffer.toString('ascii'), 'Vestibulum')
+    })
+    fixture.store.get(1, {offset: 3341}, function (_, buffer) {
+      t.equal(buffer.length, 6)
+      t.equal(buffer.toString('ascii'), 'amet. ')
+    })
+  })
+  t.end()
+})
 
-test('cleanup', function(t) {
-	t.plan(1);
-	fixture.destroy(t.ok.bind(t, true, 'should be destroyed'));
-});
+test('cleanup', function (t) {
+  t.plan(1)
+  fixture.destroy(t.ok.bind(t, true, 'should be destroyed'))
+})
--- test/tracker.js
+++ test/tracker.js
@@ -1,112 +1,111 @@
-var test = require('tap').test;
-var torrents = require('../');
-var fs = require('fs');
-var path = require('path');
-var rimraf = require('rimraf');
-var tracker = require('bittorrent-tracker');
-var server = new tracker.Server();
+var test = require('tap').test
+var torrents = require('../')
+var fs = require('fs')
+var path = require('path')
+var rimraf = require('rimraf')
+var tracker = require('bittorrent-tracker')
+var server = new tracker.Server()
 
-var torrent = fs.readFileSync(path.join(__dirname, 'data', 'test.torrent'));
-var tmpPath = path.join(__dirname, '..', 'torrents', 'test');
-rimraf.sync(tmpPath);
+var torrent = fs.readFileSync(path.join(__dirname, 'data', 'test.torrent'))
+var tmpPath = path.join(__dirname, '..', 'torrents', 'test')
+rimraf.sync(tmpPath)
 
-var fixture;
+var fixture
 
-server.on('error', function() {
-});
+server.on('error', function () {})
 
-test('seed should connect to the tracker', function(t) {
-	t.plan(3);
+test('seed should connect to the tracker', function (t) {
+  t.plan(3)
 
-	server.once('listening', function() {
-		t.ok(true, 'tracker should be listening');
-		fixture = torrents(torrent, {
-			dht: false,
-			path: path.join(__dirname, 'data')
-		});
-		fixture.listen(6882);
-		fixture.once('ready', t.ok.bind(t, true, 'should be ready'));
-	});
-	server.once('start', function(addr) {
-		t.equal(addr, '127.0.0.1:6882');
-	});
-	server.listen(12345);
-});
+  server.once('listening', function () {
+    t.ok(true, 'tracker should be listening')
+    fixture = torrents(torrent, {
+      dht: false,
+      path: path.join(__dirname, 'data')
+    })
+    fixture.listen(6882)
+    fixture.once('ready', t.ok.bind(t, true, 'should be ready'))
+  })
+  server.once('start', function (addr) {
+    t.equal(addr, '127.0.0.1:6882')
+  })
+  server.listen(12345)
+})
 
-test('peer should connect to the swarm using .torrent file', function(t) {
-	t.plan(4);
-	var engine = torrents(torrent, { dht: false });
-	engine.once('ready', function() {
-		t.ok(true, 'should be ready');
-		engine.destroy(function() {
-			engine.remove(t.ok.bind(t, true, 'should be destroyed'));
-		});
-	});
-	server.once('start', function(addr) {
-		t.equal(addr, '127.0.0.1:6881');
-	});
-	server.once('stop', function(addr) {
-		t.equal(addr, '127.0.0.1:6881');
-	});
-});
+test('peer should connect to the swarm using .torrent file', function (t) {
+  t.plan(4)
+  var engine = torrents(torrent, { dht: false })
+  engine.once('ready', function () {
+    t.ok(true, 'should be ready')
+    engine.destroy(function () {
+      engine.remove(t.ok.bind(t, true, 'should be destroyed'))
+    })
+  })
+  server.once('start', function (addr) {
+    t.equal(addr, '127.0.0.1:6881')
+  })
+  server.once('stop', function (addr) {
+    t.equal(addr, '127.0.0.1:6881')
+  })
+})
 
-test('peer should connect to the swarm using magnet link', function(t) {
-	t.plan(4);
-	var engine = torrents('magnet:?xt=urn:btih:1cb9681dccbe6ef86ac797ab93840a4f0c4ccae8' +
-		'&tr=http%3A%2F%2F127.0.0.1%3A12345%2Fannounce', { dht: false, tmp: tmpPath });
-	engine.once('ready', function() {
-		t.ok(true, 'should be ready');
-		engine.destroy(function() {
-			engine.remove(t.ok.bind(t, true, 'should be destroyed'));
-		});
-	});
-	server.once('start', function(addr) {
-		t.equal(addr, '127.0.0.1:6881');
-	});
-	server.once('stop', function(addr) {
-		t.equal(addr, '127.0.0.1:6881');
-	});
-});
+test('peer should connect to the swarm using magnet link', function (t) {
+  t.plan(4)
+  var engine = torrents('magnet:?xt=urn:btih:1cb9681dccbe6ef86ac797ab93840a4f0c4ccae8' +
+    '&tr=http%3A%2F%2F127.0.0.1%3A12345%2Fannounce', { dht: false, tmp: tmpPath })
+  engine.once('ready', function () {
+    t.ok(true, 'should be ready')
+    engine.destroy(function () {
+      engine.remove(t.ok.bind(t, true, 'should be destroyed'))
+    })
+  })
+  server.once('start', function (addr) {
+    t.equal(addr, '127.0.0.1:6881')
+  })
+  server.once('stop', function (addr) {
+    t.equal(addr, '127.0.0.1:6881')
+  })
+})
 
-test('peer should connect to the swarm using magnet link and trackers', function(t) {
-	t.plan(4);
-	var engine = torrents('magnet:?xt=urn:btih:1cb9681dccbe6ef86ac797ab93840a4f0c4ccae8',
-		{ dht: false, tmp: tmpPath, trackers: ['http://127.0.0.1:12345/announce'] });
-	engine.once('ready', function() {
-		t.ok(true, 'should be ready');
-		engine.destroy(function() {
-			engine.remove(t.ok.bind(t, true, 'should be destroyed'));
-		});
-	});
-	server.once('start', function(addr) {
-		t.equal(addr, '127.0.0.1:6881');
-	});
-	server.once('stop', function(addr) {
-		t.equal(addr, '127.0.0.1:6881');
-	});
-});
+test('peer should connect to the swarm using magnet link and trackers', function (t) {
+  t.plan(4)
+  var engine = torrents('magnet:?xt=urn:btih:1cb9681dccbe6ef86ac797ab93840a4f0c4ccae8',
+    { dht: false, tmp: tmpPath, trackers: ['http://127.0.0.1:12345/announce'] })
+  engine.once('ready', function () {
+    t.ok(true, 'should be ready')
+    engine.destroy(function () {
+      engine.remove(t.ok.bind(t, true, 'should be destroyed'))
+    })
+  })
+  server.once('start', function (addr) {
+    t.equal(addr, '127.0.0.1:6881')
+  })
+  server.once('stop', function (addr) {
+    t.equal(addr, '127.0.0.1:6881')
+  })
+})
 
-test('peer should connect to an alternate tracker', function(t) {
-	t.plan(5);
-	var server = new tracker.Server();
-	server.once('listening', function() {
-		t.ok(true, 'tracker should be listening');
-	});
-	server.once('start', function(addr) {
-		t.equal(addr, '127.0.0.1:6881');
-	});
-	server.listen(54321);
+test('peer should connect to an alternate tracker', function (t) {
+  t.plan(5)
+  var server = new tracker.Server()
+  server.once('listening', function () {
+    t.ok(true, 'tracker should be listening')
+  })
+  server.once('start', function (addr) {
+    t.equal(addr, '127.0.0.1:6881')
+  })
+  server.listen(54321)
 
-	var engine = torrents(torrent, { dht: false, trackers: ['http://127.0.0.1:54321/announce'] });
-	engine.once('ready', function() {
-		t.ok(true, 'should be ready');
-		engine.destroy(t.ok.bind(t, true, 'should be destroyed'));
-		server.close(t.ok.bind(t, true, 'tracker should be closed'));
-	});
-});
+  var engine = torrents(torrent, { dht: false, trackers: ['http://127.0.0.1:54321/announce'] })
+  engine.once('ready', function () {
+    t.ok(true, 'should be ready')
+    engine.destroy(t.ok.bind(t, true, 'should be destroyed'))
+    server.close(t.ok.bind(t, true, 'tracker should be closed'))
+  })
+})
 
-test('cleanup', function(t) {
-	t.plan(2);
-	fixture.destroy(t.ok.bind(t, true, 'should be destroyed'));
-	server.close(t.ok.bind(t, true, 'tracker should be closed'));
-});
+test('cleanup', function (t) {
+  t.plan(2)
+  fixture.destroy(t.ok.bind(t, true, 'should be destroyed'))
+  server.close(t.ok.bind(t, true, 'tracker should be closed'))
+})
